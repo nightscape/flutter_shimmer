1f18f4f01c7ebccd3c125d5c6793de05fbddca89
Martin Mauch <martin.mauch@gmail.com>
2019-07-25 00:40:01 +0200

    Make BluetoothDevice Equatable

--- lib/bluetooth_device.dart
+++ lib/bluetooth_device.dart
@@ -1,6 +1,8 @@
-class BluetoothDevice {
+import 'package:equatable/equatable.dart';
+
+class BluetoothDevice extends Equatable {
   final String name;
   final String address;
 
-  BluetoothDevice(this.name, this.address);
+  BluetoothDevice(this.name, this.address) : super([name, address]);
 }
